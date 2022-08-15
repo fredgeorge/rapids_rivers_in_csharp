0416b47e3c9e724e1289be0c59f969b06afc39ba
Fred George <fredgeorge@acm.org>
2022-08-15 13:01:26 +0200

    Fred: Handled extracting Boolean values from the Packet

--- River.Tests/Unit/PacketTest.cs
+++ River.Tests/Unit/PacketTest.cs
@@ -5,22 +5,34 @@ using Xunit;
 namespace River.Tests.Unit;
 
 public class PacketTest {
-    private readonly Packet _packet = new Packet(new Dictionary<string, object>() {
-        { "category", "car_offer_engine" },
-        { "need", "car_rental_offer" },
-        { "read_count", 3 },
-        { "empty", "" }
-    });
+    private const string Original = @"{
+        ""string_key"":""rental_offer_engine"",
+        ""integer_key"":7,
+        ""double_key"":7.5,
+        ""null_key"":null,
+        ""empty_string"":"""",
+        ""boolean_key"": true,
+        ""boolean_string_key"": ""false"",
+        ""date_time_key"": ""2022-03-03T06:06:06Z"",
+        ""string_list_key"":[""foo"",""bar""],
+        ""integer_list_key"":[2,4],
+        ""empty_list_key"":[]
+    }";
+
+    private readonly Packet _packet = new(Original);
 
     [Fact]
     public void FetchNugget() {
-        Assert.Equal("car_offer_engine", _packet["category"]);
-        Assert.Equal(3, _packet["read_count"]);
+        Assert.Equal("rental_offer_engine", _packet.String("string_key"));
+        Assert.Equal(7, _packet.Integer("integer_key"));
+        Assert.Equal(7.5, _packet.Double("double_key"));
+        Assert.True(_packet.Boolean("boolean_key"));
+        Assert.False(_packet.Boolean("boolean_string_key"));
     }
 
-    [Fact]
-    public void IsMissing() {
-        Assert.True(_packet.IsMissing("foo"));
-        Assert.True(_packet.IsMissing("empty"));
-    }
+    // [Fact]
+    // public void IsMissing() {
+    //     Assert.True(_packet.IsMissing("foo"));
+    //     Assert.True(_packet.IsMissing("empty"));
+    // }
 }
--- River/Packets/Packet.cs
+++ River/Packets/Packet.cs
@@ -1,18 +1,67 @@
+using System.Text.Json;
+
 namespace River.Packets; 
 
 // Understands a specific message on an Event Bus
 public class Packet {
-    private readonly Dictionary<string, object> _map;
+    private readonly Dictionary<string, JsonElement> _map;
 
-    public Packet(Dictionary<string, object> map) {
-        _map = map;
+    public Packet(string jsonString) {
+        if (string.IsNullOrEmpty(jsonString))
+            throw new ArgumentException("JSON string cannot be null or empty", nameof(jsonString));
+        try {
+            _map = JsonSerializer.Deserialize<Dictionary<string, JsonElement>>(jsonString)
+                                ?? throw new ArgumentException("JSON string could not be deserialized", nameof(jsonString));
+        } catch (Exception ex) {
+            throw new ArgumentException("JSON string could not be deserialized", nameof(jsonString), ex);
+        }
     }
 
-    public object this[string key] {
-        get { return _map[key]; }
-    }
+    public bool Has(string key) =>
+        _map.ContainsKey(key)
+        && (_map[key].ValueKind switch {
+            JsonValueKind.Null => false,
+            JsonValueKind.String when _map[key].GetString() == string.Empty => false,
+            _ => true
+        });
 
     public bool IsMissing(string key) {
-        return !_map.ContainsKey(key) || _map[key].Equals("");
+        return !Has(key);
+    }
+
+    public string String(string key) {
+        return Element(key, JsonValueKind.String).GetString();
+    }
+
+    public int Integer(string key) {
+        return Element(key, JsonValueKind.Number).GetInt32();
+    }
+
+    public double Double(string key) {
+        return Element(key, JsonValueKind.Number).GetDouble();
+    }
+
+    public bool Boolean(string key) {
+        if (Has(key, JsonValueKind.True)) return true;
+        if (Has(key, JsonValueKind.False)) return false;
+        if (IsBool(key, out var parsedValue)) return parsedValue;
+        throw new ArgumentException($"Value of <{key}> is of type {_map[key].ValueKind} rather than expected type of Boolean");
+    }
+
+    private JsonElement Element(string key, JsonValueKind kind) {
+        if (IsMissing(key)) throw new ArgumentException($"Key <{key}> does not exist", nameof(key));
+        if (_map[key].ValueKind != kind)
+            throw new ArgumentException($"Value of <{key}> is of type {_map[key].ValueKind} rather than expected type of {kind}", nameof(key));
+        return _map[key];
+    }
+
+    private bool Has(string key, JsonValueKind kind) {
+        return Has(key) && _map[key].ValueKind == kind;
+    }
+    
+    private bool IsBool(string key, out bool parsedValue) {
+        parsedValue = false;
+        return Has(key, JsonValueKind.String) &&
+               bool.TryParse(_map[key].GetString(), out parsedValue);
     }
 }
