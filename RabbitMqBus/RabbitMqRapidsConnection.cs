6bb3dd294040bdcb242198dc08bc9ed1e4acb99f
Fred George <fredgeorge@acm.org>
2022-08-18 09:46:47 +0200

    Fred: Added implementation of RabbitMQ as an Rapids implementation

---
+++ RabbitMqBus/RabbitMqRapidsConnection.cs
@@ -0,0 +1,74 @@
+﻿/*
+ * Copyright (c) 2022 by Fred George
+ * @author Fred George  fredgeorge@acm.org
+ * Licensed under the MIT License; see LICENSE file in root.
+ */
+
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using RapidsRivers.Packets;
+using RapidsRivers.Rapids;
+using RapidsRivers.Rivers;
+using static RapidsRivers.Rivers.River;
+
+namespace RabbitMqBus;
+
+// Understands an attachment to a Rapids using RabbitMQ
+// With RabbitMQ in "fanout" mode, you send to an Exchange, which then forwards to each Queue
+// So the Exchange serves as the Rapids, and there is one Queue per River
+// Note: In this implementation, each Service will have its own River (Rivers not shared)
+public class RabbitMqRapidsConnection : RapidsConnection {
+    private const string ExchangeName = "rapids";
+    private const string RabbitMqPubSub = "fanout";
+    private readonly IModel _channel;
+
+    public RabbitMqRapidsConnection(string host, string port) : this(host, int.Parse(port)) { }
+
+    public RabbitMqRapidsConnection(string host, int port) {
+        var factory = new ConnectionFactory { HostName = host, Port = port };
+        IConnection connection = factory.CreateConnection();
+        _channel = connection.CreateModel();
+        _channel.ExchangeDeclare(ExchangeName, RabbitMqPubSub, true, true,
+            new Dictionary<string, object>()); // Either RabbitMQ finds the exchange, or makes it
+    }
+
+    public void Register(PacketListener listener) {
+        var river = new River(this, listener.Rules, 0); // No sharing of Rivers in this implementation
+        var riverName = listener.Name; // River/Queue name can be Service name since one River per Service
+        river.Register(listener);
+        ConfigureQueueAsRiver(riverName);
+        Console.WriteLine($" [*] [service: {listener.Name}] Waiting for messages. To exit press CTRL+C");
+        ConsumeMessages(river, riverName);
+    }
+
+    public void Register(SystemListener listener) {
+        var river = new River(this, listener.Rules, 0); // No sharing of Rivers in this implementation
+        var riverName = listener.Name; // River/Queue name can be Service name since one River per Service
+        river.Register(listener);
+        ConfigureQueueAsRiver(riverName);
+        Console.WriteLine($" [*] [service: {listener.Name}] Waiting for messages. To exit press CTRL+C");
+        ConsumeMessages(river, riverName);
+    }
+
+    public void Publish(RapidsPacket packet) {
+        var body = Encoding.UTF8.GetBytes(packet.ToJsonString());
+        _channel.BasicPublish(exchange: ExchangeName, routingKey: "", basicProperties: null, body: body);
+    }
+
+    private void ConfigureQueueAsRiver(string riverName) {
+        _channel.QueueDeclare(riverName, false, true, true, null);
+        _channel.QueueBind(riverName, ExchangeName, "");
+    }
+
+    private void ConsumeMessages(River river, string riverName) {
+        var consumer = new EventingBasicConsumer(_channel); // Define consumer function triggered on each message
+        consumer.Received += (_, ea) => {
+            var body = ea.Body.ToArray();
+            var jsonString = Encoding.Default.GetString(body);
+            river.Message(this, jsonString);
+            _channel.BasicAck(ea.DeliveryTag, false);
+        };
+        _channel.BasicConsume(riverName, false, consumer); // Start consuming
+    }
+}
---
+++ Sample/MonitorService/Monitor.cs
@@ -0,0 +1,42 @@
+﻿/*
+ * Copyright (c) 2022 by Fred George
+ * @author Fred George  fredgeorge@acm.org
+ * Licensed under the MIT License; see LICENSE file in root.
+ */
+
+using RabbitMqBus;
+using RapidsRivers.Packets;
+using RapidsRivers.Rapids;
+using RapidsRivers.Rivers;
+using RapidsRivers.Validation;
+using static RapidsRivers.Rivers.River;
+
+namespace MonitorService;
+
+public class Monitor : PacketListener {
+    public string Name => $"Monitor [{GetHashCode()}]";
+    // Sample rule possibilities in comments below:
+    public Rules Rules => new(
+        // new RequireValue("key_for_string", "expectedValue"),  // This requires Packet to have key-value pair
+        // new RequireValue("key_for_number", 42.42),  // This requires Packet to have key-value pair
+        // new RequireKeys("key1", "key2", "key3"),  // This requires these keys exist (doesn't care about values)
+        // new ForbidKeys("key4", "key5")  // This forbids these keys existing. Note that 'null', empty string, and empty arrays are considered to not exist
+        );
+
+    public static void Main(string[] args)  // Pass in <IP address> and <port> for RabbitMQ
+    {
+        var rapidsConnection = new RabbitMqRapidsConnection(args[0], args[1]);
+        rapidsConnection.Register(new Monitor());
+        Thread.Sleep(Timeout.Infinite);
+    }
+
+    public bool IsStillAlive(RapidsConnection connection) => true;
+
+    public void Packet(RapidsConnection connection, Packet packet, Status information) {
+        Console.WriteLine(" [x] {0}", information);
+    }
+
+    public void RejectedPacket(RapidsConnection connection, Packet packet, Status problems) {
+        Console.WriteLine(" [x] {0}", problems);
+    }
+}
